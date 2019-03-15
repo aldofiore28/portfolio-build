ab7770b32594ce5978013ea03a642d36972c63b6
aldofiore28 <aldo.fiore95@gmail.com>
2019-03-15 16:37:29 +0000

    changed files names, added admin page and styles

---
+++ adminPage.php
@@ -0,0 +1,27 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link rel="stylesheet" href="styles/normalize.css" type="text/css" />
+        <link rel="stylesheet" href="styles/adminPageStyle.css" type="text/css" />
+        <title>Admin Page</title>
+    </head>
+    <body>
+        <form action="#" method="post">
+           <input type="submit" name="addButton" value="Add" />
+        </form>
+        <form action="#" method="post">
+            <select name="aboutMeSectionEditing">
+
+            </select>
+            <input type="submit" name="editButton" value="Edit" />
+        </form>
+        <form action="#" method="post">
+            <select name="aboutMeSectionRemoval">
+
+            </select>
+            <input type="submit" name="removeButton" value="Remove" />
+        </form>
+    </body>
+</html>
