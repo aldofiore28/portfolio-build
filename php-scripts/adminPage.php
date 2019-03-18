45194f8fe203feb5b03c2e585dc0c688ee071fa6
aldofiore28 <aldo.fiore95@gmail.com>
2019-03-18 10:35:51 +0000

    refined styles in the admin page

--- php-scripts/adminPage.php
+++ php-scripts/adminPage.php
@@ -11,19 +11,19 @@
         <main>
             <h1>CMS Portfolio</h1>
             <form action="addContent.php" method="post">
-               <input type="submit" name="addButton" value="Add" />
+               <input class="sizeContentChangeButton" type="submit" name="addButton" value="Add" />
             </form>
             <form action="editContent.php" method="post">
                 <select name="aboutMeSectionEditing">
 
                 </select>
-                <input type="submit" name="editButton" value="Edit" />
+                <input class="sizeContentChangeButton" type="submit" name="editButton" value="Edit" />
             </form>
             <form action="#" method="post">
                 <select name="aboutMeSectionRemoval">
 
                 </select>
-                <input type="submit" name="removeButton" value="Remove" />
+                <input class="sizeContentChangeButton" type="submit" name="removeButton" value="Remove" />
             </form>
             <form action="../index.php">
                 <input type="submit" value="Go to Main Page"/>
