8b1161c842133c40a3ad8295bd4751e456912478
aldofiore28 <aldo.fiore95@gmail.com>
2019-03-18 10:24:37 +0000

    Styled the add content page

--- php-scripts/addContent.php
+++ php-scripts/addContent.php
@@ -4,11 +4,22 @@
 <html lang="en">
 
     <head>
+        <link rel="stylesheet" href="../styles/normalize.css" type="text/css" />
+        <link rel="stylesheet" href="../styles/addEditStyles.css" type="text/css" />
         <title>Add Content</title>
     </head>
 
     <body>
-
+        <main>
+            <h1>Add Content</h1>
+            <form method="post" action="addContent.php">
+                <textarea cols="10" rows="10" placeholder="New Content..."></textarea>
+                <input type="submit" value="Add!" />
+            </form>
+            <form action="adminPage.php">
+                <input class="goBackButton" type="submit" value="Go back to admin Page!">
+            </form>
+        </main>
     </body>
 
 </html>
