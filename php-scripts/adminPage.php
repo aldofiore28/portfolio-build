08d41972fe97680c01a305b2d26d370dc643d3f8
aldofiore28 <aldo.fiore95@gmail.com>
2019-03-18 10:44:15 +0000

    Changed a form for a button with a link

--- php-scripts/adminPage.php
+++ php-scripts/adminPage.php
@@ -11,23 +11,23 @@
         <main>
             <h1>CMS Portfolio</h1>
             <form action="addContent.php" method="post">
-               <input class="sizeContentChangeButton" type="submit" name="addButton" value="Add" />
+               <input type="submit" name="addButton" value="Add" />
             </form>
             <form action="editContent.php" method="post">
                 <select name="aboutMeSectionEditing">
 
                 </select>
-                <input class="sizeContentChangeButton" type="submit" name="editButton" value="Edit" />
+                <input type="submit" name="editButton" value="Edit" />
             </form>
             <form action="#" method="post">
                 <select name="aboutMeSectionRemoval">
 
                 </select>
-                <input class="sizeContentChangeButton" type="submit" name="removeButton" value="Remove" />
+                <input type="submit" name="removeButton" value="Remove" />
             </form>
-            <form action="../index.php">
-                <input type="submit" value="Go to Main Page"/>
-            </form>
+            <button>
+                <a href="../index.php">Go to Main Page</a>
+            </button>
         </main>
     </body>
 </html>
