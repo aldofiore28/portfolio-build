0e2b90b41bae6cee3f00a615af49da6790f94c6d
aldofiore28 <aldo.fiore95@gmail.com>
2019-03-19 15:01:06 +0000

    Added more docblocks

--- php-scripts/functions/queryEditContent.php
+++ php-scripts/functions/queryEditContent.php
@@ -1,5 +1,16 @@
 <?php
 
+/**
+ * The following function will run an SQL query that will change the text selected from the admin page
+ * with a new one that you insert in the text area. The textarea populates from the database, so you can
+ * change a letter if you want.
+ *
+ * @param PDO $db Database connection
+ * @param string $idOldText id from the text that you need to change
+ * @param string $newText new text to save in the db
+ *
+ * @return bool Returns if the query has been executed or not.
+ */
 function updateSelectedText (PDO $db, string $idOldText, string $newText) : bool {
     $query = $db->prepare('UPDATE `about_me_section` SET `section` = :newText WHERE `id`=:idOldText');
     $query->bindParam(':idOldText',$idOldText);
