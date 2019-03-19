63572e6503a1439a22774a57a9ad32609c9f3590
aldofiore28 <aldo.fiore95@gmail.com>
2019-03-19 15:10:51 +0000

    docblocks for the function populateDropdown

--- php-scripts/functions/populateDropdown.php
+++ php-scripts/functions/populateDropdown.php
@@ -1,11 +1,29 @@
 <?php
 
+
+/**
+ * The following function will run a SQL query that will take the id and description from the database
+ *
+ * @param PDO $db Database connection
+ *
+ * @return array Returns an indexed array of associative arrays that we will use to populate the
+ * dropdowns.
+ */
 function getItemsFromDB (PDO $db) {
     $query = $db->prepare('SELECT `id`, `section` FROM `about_me_section` WHERE `deleted`=0;');
     $query->execute();
     return $query->fetchAll();
 }
 
+/**
+ * The following function will concatenate all the information in the array into HTML tags:
+ * The id will become the attribute of the option TAG 'value'.
+ * The section will become the visual input that you get from the dropdown.
+ *
+ * @param array $array array retrieved from the database
+ *
+ * @return string Returns a concatenated string of HTML tags.
+ */
 function populateDropdownEdit(array $array) :string {
     $result ='';
     foreach ($array as $value) {
